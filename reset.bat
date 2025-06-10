113e12a3abb29bed7afd614160f9c51bc3de4352
kemery919 <kyle4681@hotmail.com>
2025-06-10 17:53:04 -0500

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -5,6 +5,10 @@
 :: PowerShell/Bash script to delete and run the migrations 
 :: and let you know when to run the SQL Query
 
+:: -------------------------------------------------------
+:: -- Replace all <Variables> with your own information --
+:: -------------------------------------------------------
+
 :: Use the below to run this (*** As long as it is named "reset.bat" ***)
 
 :: To run in bash terminal use:
